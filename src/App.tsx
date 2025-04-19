6cd8dbd989fa378a51c60eb90d49cfe18f5935a7
George Slanskiy <124250538+georgeslnc@users.noreply.github.com>
2025-04-19 12:17:18 +0300

    add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { api } from './axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+
+  useEffect(() => {
+    api.get('/projects').then((res) => {
+      console.log(res)
+    })
+  },[])
 
   return (
     <>
